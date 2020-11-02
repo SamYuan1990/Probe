a08414d35daa8d403cf4514b677d67a7481a4318
SamYuan1990 <yy19902439@126.com>
2020-11-02 22:50:16 +0800

    refactor
    Signed-off-by: SamYuan1990 <yy19902439@126.com>

---
+++ test/fileIO.test.js
@@ -0,0 +1,66 @@
+const fileIO = require('../lib/fileIO');
+var expect = require('chai').expect;
+
+describe('# fileIO', function () {
+    it('appendRS', function(done){
+        fileIO.init();
+        fileIO.appendRS('sample,0.75,10,2,256, 180.038278,');
+        fileIO.appendRS('sample,0.75,40,2,256, 291.310916,');
+        fileIO.appendRS('sample,0.75,80,2,256, 333.041573,');
+        fileIO.appendRS('sample,0.75,120,2,256, 351.752320,');
+        fileIO.appendRS('sample,1,10,2,256, 172.872861,');
+        fileIO.appendRS('sample,1,40,2,256, 291.617799,');
+        fileIO.appendRS('sample,1,80,2,256, 337.826232,');
+        fileIO.appendRS('sample,1,120,2,256, 319.039588,');
+        fileIO.appendRS('sample,2,10,2,256, 182.105577,');
+        fileIO.appendRS('sample,2,40,2,256, 260.276446,');
+        fileIO.appendRS('sample,2,80,2,256, 323.542760,');
+        fileIO.appendRS('sample,2,120,2,256, 323.526945,');
+        fileIO.appendRS('sample,1.5,10,2,256, 172.745382,');
+        fileIO.appendRS('sample,1.5,40,2,256, 268.041591,');
+        fileIO.appendRS('sample,1.5,80,2,256, 348.150198,');
+        fileIO.appendRS('sample,1.5,120,2,256, 310.915616,');
+        done();
+    })
+
+    it('get BatchTimeout', function (done) {
+        expect([0.75,0.75,0.75,0.75,1,1,1,1,2,2,2,2,1.5,1.5,1.5,1.5]).to.deep.equal(
+            fileIO.loadRs(fileIO.BatchTimeout));
+        done();
+    })
+    it('get MaxMessageCount', function (done) {
+        expect([10,40,80,120,10,40,80,120,10,40,80,120,10,40,80,120]).to.deep.equal(
+            fileIO.loadRs(fileIO.MaxMessageCount));
+        done();
+    })
+    it('get AbsoluteMaxBytes', function (done) {
+        expect([2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2]).to.deep.equal(
+            fileIO.loadRs(fileIO.AbsoluteMaxBytes));
+        done();
+    })
+    it('get PreferredMaxBytes', function (done) {
+        expect([256,256,256,256,256,256,256,256,256,256,256,256,256,256,256,256]).to.deep.equal(
+            fileIO.loadRs(fileIO.PreferredMaxBytes));
+        done();
+    })
+    it('get TPS', function (done) {
+        expect([180.038278,
+            291.310916,
+            333.041573,
+            351.752320,
+            172.872861,
+            291.617799,
+            337.826232,
+            319.039588,
+            182.105577,
+            260.276446,
+            323.542760,
+            323.526945,
+            172.745382,
+            268.041591,
+            348.150198,
+            310.915616]).to.deep.equal(
+            fileIO.loadRs(fileIO.TPS));
+        done();
+    })
+})
--- test/lib.test.js
+++ test/lib.test.js
@@ -2,82 +2,65 @@ const libs = require('../lib/libs');
 var expect = require('chai').expect;
 
 describe('# libs', function () {
-    it('run', function(done){
-        CmdInfo = {
-            Chaincode:'sample',
-            CoolDown: 1,
-            PrepareCLI: 'echo',
-            StartCLI: 'echo',
-            DryRun: true,
-            ShutDownCLI: 'echo',
-        }
-        BatchTimeout = [2];
-        MaxMessageCount = [20];
-        AbsoluteMaxBytes= [10];
-        PreferredMaxBytes= [10];
-        expect(true).to.deep.equal(libs.run(CmdInfo,BatchTimeout,MaxMessageCount,AbsoluteMaxBytes,PreferredMaxBytes)===0);
-        done();
+    context('run', function(){
+        it('run', function(done){
+            CmdInfo = {
+                Chaincode:'sample',
+                CoolDown: 1,
+                PrepareCLI: 'echo',
+                StartCLI: 'echo',
+                DryRun: true,
+                ShutDownCLI: 'echo',
+                Path: '.',
+            }
+            BatchTimeout = [2];
+            MaxMessageCount = [20];
+            AbsoluteMaxBytes= [10];
+            PreferredMaxBytes= [10];
+            expect(0).to.deep.equal(
+                libs.Run(CmdInfo,BatchTimeout,MaxMessageCount,AbsoluteMaxBytes,PreferredMaxBytes));
+            done();
+        })
     })
 
-    it('appendRS', function(done){
-        libs.init();
-        libs.appendRS('sample,0.75,10,2,256, 180.038278,');
-        libs.appendRS('sample,0.75,40,2,256, 291.310916,');
-        libs.appendRS('sample,0.75,80,2,256, 333.041573,');
-        libs.appendRS('sample,0.75,120,2,256, 351.752320,');
-        libs.appendRS('sample,1,10,2,256, 172.872861,');
-        libs.appendRS('sample,1,40,2,256, 291.617799,');
-        libs.appendRS('sample,1,80,2,256, 337.826232,');
-        libs.appendRS('sample,1,120,2,256, 319.039588,');
-        libs.appendRS('sample,2,10,2,256, 182.105577,');
-        libs.appendRS('sample,2,40,2,256, 260.276446,');
-        libs.appendRS('sample,2,80,2,256, 323.542760,');
-        libs.appendRS('sample,2,120,2,256, 323.526945,');
-        libs.appendRS('sample,1.5,10,2,256, 172.745382,');
-        libs.appendRS('sample,1.5,40,2,256, 268.041591,');
-        libs.appendRS('sample,1.5,80,2,256, 348.150198,');
-        libs.appendRS('sample,1.5,120,2,256, 310.915616,');
-        done();
+    context('handleStatus', function(){
+        it('should success', function(done){
+            rs = {
+                status: 0,
+            }
+            expect(0).to.be.equals(libs.handleStatus(rs));
+            done();
+        })
+
+        it('should handle error', function(done){
+            rs = {
+                status: 1,
+                stderr: 'abc',
+            }
+            expect(-1).to.be.equals(libs.handleStatus(rs));
+            done();
+        })
     })
 
-    it('get BatchTimeout', function (done) {
-        expect([0.75,0.75,0.75,0.75,1,1,1,1,2,2,2,2,1.5,1.5,1.5,1.5]).to.deep.equal(
-            libs.loadRs(libs.BatchTimeout));
-        done();
-    })
-    it('get MaxMessageCount', function (done) {
-        expect([10,40,80,120,10,40,80,120,10,40,80,120,10,40,80,120]).to.deep.equal(
-            libs.loadRs(libs.MaxMessageCount));
-        done();
-    })
-    it('get AbsoluteMaxBytes', function (done) {
-        expect([2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2]).to.deep.equal(
-            libs.loadRs(libs.AbsoluteMaxBytes));
-        done();
-    })
-    it('get PreferredMaxBytes', function (done) {
-        expect([256,256,256,256,256,256,256,256,256,256,256,256,256,256,256,256]).to.deep.equal(
-            libs.loadRs(libs.PreferredMaxBytes));
-        done();
-    })
-    it('get TPS', function (done) {
-        expect([180.038278,
-            291.310916,
-            333.041573,
-            351.752320,
-            172.872861,
-            291.617799,
-            337.826232,
-            319.039588,
-            182.105577,
-            260.276446,
-            323.542760,
-            323.526945,
-            172.745382,
-            268.041591,
-            348.150198,
-            310.915616]).to.deep.equal(
-            libs.loadRs(libs.TPS));
-        done();
+    context('executeCommand', function(){
+        it('should success if dry run', function(done){
+            rs = {
+                DryRun: true,
+            }
+            expect(0).to.be.equals(libs.executeCommand(command));
+            done();
+        })
+
+        it('should success', function(done){
+            rs = {
+                DryRun: false,
+                info: 'echo',
+                command: 'echo',
+                args: ['a'],
+                Path: './s'
+            }
+            expect(0).to.be.equals(libs.executeCommand(command));
+            done();
+        })
     })
 })
