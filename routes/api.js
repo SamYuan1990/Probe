263e200bcc365783dd1b017f6fbd6bba5625308b
SamYuan1990 <yy19902439@126.com>
2020-11-03 22:55:39 +0800

    adding logger
    Signed-off-by: SamYuan1990 <yy19902439@126.com>

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const libs = require('../lib/libs');
 const fileIO = require('../lib/fileIO');
+const log4js = require('log4js');
+const logger = log4js.getLogger('app');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -40,7 +42,7 @@ function prepareArray(input) {
 router.get('/run', function(req, res, next) {
     let status = 0;
     fileIO.init();
-    console.log(req.query);
+    logger.log(req.query);
     const CmdInfo = {
         Chaincode:'sample',
         Path: req.query.Path,
@@ -60,13 +62,13 @@ router.get('/run', function(req, res, next) {
     const AbsoluteMaxBytes = prepareArray(req.query.AbsoluteMaxBytes);
     const PreferredMaxBytes = prepareArray(req.query.PreferredMaxBytes);
     const d = new Date();
-    console.log('process start');
+    logger.log('process start');
     status = libs.Run(CmdInfo, BatchTimeout, MaxMessageCount, AbsoluteMaxBytes, PreferredMaxBytes);
     if (status === 0) {
-        console.log(new Date().toString());
+        logger.log(new Date().toString());
         res.send(d.toString() + ' success at ' + new Date().toString());
     } else {
-        console.log('error');
+        logger.error('error');
         res.sendStatus(500);
     }
 });
