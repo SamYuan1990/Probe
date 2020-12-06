5ecfc5542dc0fb331bd219095a9b745bcfc7379e
SamYuan1990 <yy19902439@126.com>
2020-12-06 16:46:49 +0800

    fix up
    Signed-off-by: SamYuan1990 <yy19902439@126.com>

--- routes/quick.js
+++ routes/quick.js
@@ -11,7 +11,7 @@ router.get('/config', function(req, res, next) {
 
 router.get('/BatchTimeout', function(req, res, next) {
     logger.info(req.query);
-    const url = '/api/run?Path=.%2Ffabric-samples%2Ftest-network%2F&BatchTimeout=0.125%2C0.25%2C0.5%2C1%2C2%2C4%2C8&MaxMessageCount=50000&AbsoluteMaxBytes=2&PreferredMaxBytes=4&CoolDown=5&PrepareCLI=.%2FprepareConfig.sh&StartCLI=.%2Fnetwork.sh&CCDeployCLI=.%2Fnetwork.sh&TapeCount=50000&ShutDownCLI=.%2Fnetwork.sh';
+    const url = '/api/run?Path=.%2Ffabric-samples%2Ftest-network%2F&BatchTimeout=0.125%2C0.25%2C0.5%2C1%2C2%2C4%2C8&MaxMessageCount=50000&AbsoluteMaxBytes=20&PreferredMaxBytes=20480&CoolDown=5&PrepareCLI=.%2FprepareConfig.sh&StartCLI=.%2Fnetwork.sh&CCDeployCLI=.%2Fnetwork.sh&TapeCount=50000&ShutDownCLI=.%2Fnetwork.sh';
     if (req.query.DryRun) {
         res.redirect(url + '&DryRun=true');
     } else {
