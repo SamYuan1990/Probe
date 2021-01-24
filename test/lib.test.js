97debce7e9be84fbda0b0f2aba59479834d5ad13
SamYuan1990 <yy19902439@126.com>
2021-01-24 22:09:23 +0800

    make new UI works
    Signed-off-by: SamYuan1990 <yy19902439@126.com>

--- test/lib.test.js
+++ test/lib.test.js
@@ -104,4 +104,17 @@ describe('# libs', function () {
             done();
         });
     });
+
+    context('runNew', function() {
+        it('run success', function(done) {
+            const cmds = JSON.parse('[{"order":0,"cmdType":"PrePare","args":["./prepareConfig.sh"]},{"order":1,"cmdType":"Shell","args":["./network.sh","up","createChannel","-i","2.2"]},{"order":2,"cmdType":"Shell","args":["./network.sh","deployCC","-d","5","-ccn","basic","-ccp","../asset-transfer-basic/chaincode-go/","-ccl","go"]},{"order":3,"cmdType":"Shell","args":["sleep","10"]},{"order":4,"cmdType":"Tape","args":["docker","run","--name","tape","-e","TAPE_LOGLEVEL=debug","--network","host","-v","./:/config","guoger/tape","tape","-c","/config/config.yaml","-n","500"]},{"order":5,"cmdType":"Shell","args":["docker","rm","tape"]},{"order":6,"cmdType":"Shell","args":["./network.sh","down"]},{"order":7,"cmdType":"Shell","args":["sleep","10"]}]');
+            const BatchTimeout = [2];
+            const MaxMessageCount = [20];
+            const AbsoluteMaxBytes = [10];
+            const PreferredMaxBytes = [10];
+            expect(0).to.deep.equal(
+                libs.RunNew(BatchTimeout, MaxMessageCount, AbsoluteMaxBytes, PreferredMaxBytes, true, './', cmds));
+            done();
+        });
+    });
 });
